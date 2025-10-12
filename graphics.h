c4794bbc985a64fcfe6a3ee1342dc6f642ec731a
Aditya-B-Dhruva <131037390+Aditya-B-007@users.noreply.github.com>
2025-10-12 23:21:40 +0530

    Add files via upload

---
+++ graphics.h
@@ -0,0 +1,51 @@
+#ifndef GRAPHICS_H
+#define GRAPHICS_H
+#include <stdint.h>
+typedef struct __attribute__((packed)){
+    uint16_t attributes;
+    uint8_t windowA, windowB;
+    uint16_t granularity;
+    uint16_t windowSize;
+    uint16_t windowASegment, windowBSegment;
+    uint32_t winFuncPtr;
+    uint16_t pitch;
+    uint16_t resolution_x, resolution_y;
+    uint8_t wChar, yChar, planes, bitsPerPixel,banks;
+    uint8_t memoryModel, bankSize, imagePages;
+    uint8_t reserved1;
+    uint8_t redMask, redMaskPosition;
+    uint8_t greenMask, greenMaskPosition;
+    uint8_t blueMask, blueMaskPosition;
+    uint8_t rsvdMask, rsvdMaskPosition;
+    uint8_t directColorAttributes;
+    uint32_t physbase; 
+    uint32_t reserved2;
+    uint16_t reserved3;
+} VbeModeInfo;
+typedef struct{
+    void* address;
+    uint32_t        width;
+    uint32_t        height;
+    uint32_t        pitch;
+    uint8_t         bitsPerPixel;
+    uint8_t         bytesPerPixel;
+} FrameBuffer;
+typedef struct {
+    uint32_t width;
+    uint32_t height;
+    uint8_t  data;
+}Bitmap;
+typedef struct {
+    Bitmap* bitmap;
+    uint32_t char_width;
+    uint32_t char_height;
+}Font;
+void clear_screen(FrameBuffer* fb, uint32_t color);
+void put_pixel(FrameBuffer* fb, uint32_t x, uint32_t y, uint32_t color);
+void draw_line(FrameBuffer* fb, uint32_t x0, uint32_t y0, uint32_t x1, uint32_t y1, uint32_t color);
+void draw_rectangle(FrameBuffer* fb, uint32_t x, uint32_t y, uint32_t width, uint32_t height, uint32_t color);
+void draw_circle(FrameBuffer* fb, uint32_t x, uint32_t y, uint32_t radius, uint32_t color);
+void draw_bitmap(FrameBuffer* fb, Bitmap* bmp, uint32_t x, uint32_t y, uint32_t color);
+void draw_char(FrameBuffer* fb, Font* font, char c, uint32_t x, uint32_t y, uint32_t color);
+void draw_string(FrameBuffer* fb, Font* font, const char* str, uint32_t x, uint32_t y, uint32_t color);
+#endif
--- kernel.c
+++ kernel.c
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include "graphics.h"
 #include "vga.h"
 #include "idt.h"
 #include "gdt.h"
@@ -17,6 +18,26 @@ void counter_task(){
         vga_putentryat('A' + (i++%26),0x0F,79,0);
     }
 }
+
+// Assuming vbe_mode_info is provided by the bootloader.
+// This is a common structure for VBE mode information.
+typedef struct {
+    uint16_t attributes;
+    uint8_t window_a, window_b;
+    uint16_t granularity;
+    uint16_t window_size;
+    uint16_t segment_a, segment_b;
+    uint32_t win_func_ptr;
+    uint16_t pitch, resolution_x, resolution_y;
+    uint8_t chars_x, chars_y, planes, bitsPerPixel, banks;
+    uint8_t memory_model, bank_size, image_pages;
+    uint8_t reserved0;
+    // ... other fields might follow
+    uint32_t physbase;
+} __attribute__((packed)) vbe_mode_info_t;
+
+extern vbe_mode_info_t vbe_mode_info;
+
 void kernel_main(void) {
     vga_init();
     vga_setcolor(vga_entry_color(VGA_COLOR_WHITE, VGA_COLOR_BLACK));
@@ -42,10 +63,23 @@ void kernel_main(void) {
     vga_print_string("\nKernel initialization complete!\n");
     vga_print_string("Security features: Buffer overflow protection, bounds checking\n");
     vga_print_string("Starting shell...\n\n");
-    
-    shell_init();
+    FrameBuffer fb;
+    fb.address=(void*)vbe_mode_info.physbase;
+    fb.width=vbe_mode_info.resolution_x;
+    fb.height=vbe_mode_info.resolution_y;
+    fb.pitch=vbe_mode_info.pitch;
+    fb.bitsPerPixel=vbe_mode_info.bitsPerPixel;
+    fb.bytesPerPixel = vbe_mode_info.bitsPerPixel / 8;
+    if (fb.bitsPerPixel != 24) {
+        vga_print_string("Error: Graphics mode is not 24-bit color depth!\n");
+        for(;;) { asm volatile("cli; hlt"); }
+    }
+    uint32_t xc = fb.width / 2;
+    uint32_t yc = fb.height / 2;
 
-    // Main kernel loop - wait for interrupts
+    clear_screen(&fb, 0x000000);
+    draw_circle(&fb, xc, yc, 50,  0xFFFFFF);
+    //Scheduler takes over, chillax!!
     while(1) {
         asm volatile("hlt");
     }
